4e5d1e22faaa55f59fa251916fc221fc1d8c6125
Rafael Moreira <rafa.siffert@gmail.com>
2023-08-21 13:43:02 -0400

    artihmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31 ;
+let num2 = 2;
+const multiply = num1*num2;
+
+
+const random = Math.floor(Math.random()* 100)+ 1;
+
+const numbers = [10,5,20]
+const max = Math.max (...numbers);
+
+let num3 = 17;
+let num4 = 13;
+const mod = num3 % num4;
